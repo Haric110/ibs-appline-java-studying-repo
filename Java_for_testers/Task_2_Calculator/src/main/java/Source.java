ec6cf9d080eaf3008d0f51cc2e7837f44d534cc9
Александров Игорь Владимирович <Igor.Aleksandrov@appline.ru>
2021-02-07 00:45:51 +0300

    Добавлено задание 2.

---
+++ Java_for_testers/Task_2_Calculator/src/main/java/Source.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Source {
+
+    /**
+     * @param a число с плавающей точкой
+     * @param b число с плавающей точкой
+     * @return сумма аргументов a и b
+     *
+     * Возвращает сумму аргументов типа данных double.
+     */
+    public static double summer(double a, double b){
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a, b;
+
+        System.out.println("Введите дробные числа a и b (в качестве разделителя дробной части используйте ','):");
+        System.out.print("а = ");
+        a = scanner.nextDouble();
+        System.out.print("b = ");
+        b = scanner.nextDouble();
+
+        System.out.printf("Результат суммы: %.4f", summer(a, b));
+    }
+}
