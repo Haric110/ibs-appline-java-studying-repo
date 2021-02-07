37b584928b97a973263a1f3bd4dca14e54fb6cb4
Александров Игорь Владимирович <Igor.Aleksandrov@appline.ru>
2021-02-07 05:12:37 +0300

    Реализован Определитель Длиннющих слов для задания 3.

--- Java_for_testers/Task_3/src/main/java/Source.java
+++ Java_for_testers/Task_3/src/main/java/Source.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Source {
     public static void main (String[] args) {
-        Calculator.userInterface();
+
+        System.out.println("""
+                Добро пожаловать в программу, в которой у Вас есть выбор!
+                Ваши опции:
+                 введите 1 для вызова Калькулятора;
+                 введите 2 для вызова Определителя Длиннющих Слов.
+                """);
+
+        System.out.print("И так, Ваш выбор: ");
+
+        switch (new Scanner(System.in).next()){
+            case "1":
+                Calculator.userInterface();
+                break;
+            case "2":
+                WorksWithWords.userInterface();
+                break;
+            default:
+                System.out.println("Вы сделали неправильный выбор. Прощайте.");
+        }
     }
 }
--- Java_for_testers/Task_3/src/main/java/WorksWithWords.java
+++ Java_for_testers/Task_3/src/main/java/WorksWithWords.java
@@ -1,3 +1,54 @@
-public class WorksWithWords {
+import java.util.Scanner;
 
+public class WorksWithWords {
+    /**
+     * Предлагает ввести количество слов, затем ввести сами слова через ENTER, после чего определяет
+     * и выводит наиболее длинное из них.
+     * Если в строке ввода есть несколько слов, читается только первое из них.
+     */
+    public static void userInterface() {
+        final int WORDS_COUNT;
+
+        int max_length, indexOfTheLongestWord;
+
+        final String[] words;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("\nДобро пожаловать в Определитель Длиннющих Слов! Вы сделали правильный выбор!\n\n");
+
+        System.out.print("Введите число слов: ");
+        WORDS_COUNT = scanner.nextInt();
+        scanner.nextLine();
+
+        if (WORDS_COUNT <= 0) {
+            System.out.println("""
+                    Некорректное число элементов списка
+                    Определитель Длиннющих Слов окончил свою работу.""");
+            System.exit(0);
+        }
+
+        words = new String[WORDS_COUNT];
+        System.out.println("Теперь введите их! После каждого слова нажимайте ENTER.");
+
+        max_length = 0;
+        indexOfTheLongestWord = 0;
+
+        for (int i = 0; i < WORDS_COUNT; i++) {
+            System.out.printf("слово_%d = ", i + 1);
+
+            words[i] = scanner.nextLine().split(" ")[0];
+
+            if (words[i].length() > max_length) {
+                max_length = words[i].length();
+                indexOfTheLongestWord = i;
+            }
+        }
+
+        System.out.printf("\nДлиннющее слово: \"%s\"\n\n", words[indexOfTheLongestWord]);
+        System.out.println("""
+                Спасибо, что пользуетесь "Определителем Длиннющих Слов"!
+                Всего доброго!""");
+    }
 }
